7a327bfdce807ac6dd6e5ca4abfd3892cf847629
Daquiver1 <Cabrokwa11@gmail.com>
2023-08-26 20:59:23 +0000

    inital commit

---
+++ scheduled.py
@@ -0,0 +1,49 @@
+import os
+import schedule
+import time
+import logging
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+logging.basicConfig(level=logging.DEBUG)
+CHANNEL_ID = "C05N52FH0SF"
+
+
+def sendMessage(slack_client):
+    # make the POST request through the python slack client
+
+    # check if the request was a success
+    try:
+        response = slack_client.conversations_members(channel=CHANNEL_ID)
+        if response["ok"]:
+            user_ids = response["members"]
+            user_ids.remove("U05QD5SS81W")
+        else:
+            print(f"Error getting user list: {response['error']}")
+        for user_id in user_ids:
+            slack_client.chat_postMessage(
+                channel="#faang-soon",
+                text=f"Hello <@{user_id}>, hope you are good. Please send in your update.",
+            )
+
+    except SlackApiError as e:
+        logging.error("Request to Slack API Failed: {}.".format(e.response.status_code))
+        logging.error(e.response)
+
+
+if __name__ == "__main__":
+    SLACK_BOT_TOKEN = "xoxb-3769684266272-5829196892064-rCWQ064aWXBZnjru5N1LEEsQ"  # os.environ["SLACK_BOT_TOKEN"]
+    slack_client = WebClient(SLACK_BOT_TOKEN)
+    logging.debug("authorized slack client")
+
+    # # For testing
+    # msg = "<@U043TGDDV7G|Baaba>, <@U04QE7S6AHY|Senpai>, <@U04R0GQGEBB|Nai>, please send in your update."
+    # schedule.every(10).seconds.do(lambda: sendMessage(slack_client))
+    sendMessage(slack_client)
+
+    # schedule.every().monday.at("13:15").do(lambda: sendMessage(slack_client, msg))
+    logging.info("entering loop")
+
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(5)  # sleep for 5 seconds between checks on the scheduler
---
+++ server.py
@@ -0,0 +1,56 @@
+import os
+import json
+import logging
+
+from flask import Flask, request, make_response, Response
+
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_sdk.signature import SignatureVerifier
+
+from slashCommand import Slash
+
+logging.basicConfig(level=logging.DEBUG)
+app = Flask(__name__)
+
+@app.route("/slack/test", methods=["POST"])
+def command():
+  if not verifier.is_valid_request(request.get_data(), request.headers):
+    return make_response("invalid request", 403)
+  info = request.form
+
+  # # send user a response via DM
+  # im_id = slack_client.im_open(user=info["user_id"])["channel"]["id"]
+  # ownerMsg = slack_client.chat_postMessage(
+  #   channel=im_id,
+  #   text=commander.getMessage()
+  # )
+
+  # # send channel a response
+  # response = slack_client.chat_postMessage(
+  #   channel='#{}'.format(info["channel_name"]), 
+  #   text=commander.getMessage()
+  # )
+
+  try:
+    response = slack_client.chat_postMessage(
+      channel='#{}'.format(info["channel_name"]), 
+      text=commander.getMessage()
+    )#.get()
+  except SlackApiError as e:
+    logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
+    logging.error(e.response)
+    return make_response("", e.response.status_code)
+
+  return make_response("", response.status_code)
+
+# Start the Flask server
+if __name__ == "__main__":
+  SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+  SLACK_SIGNATURE = os.environ['SLACK_SIGNATURE']
+  slack_client = WebClient(SLACK_BOT_TOKEN)
+  verifier = SignatureVerifier(SLACK_SIGNATURE)
+
+  commander = Slash("Hey there! It works.")
+
+  app.run()
